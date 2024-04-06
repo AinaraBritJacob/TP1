7c2ca8d67f7121eb7f0ae9b3cd2411c309762ab8
AinaraBritJacob <166017112+AinaraBritJacob@users.noreply.github.com>
2024-04-06 17:20:08 -0300

    Add files via upload

---
+++ Ainara Visual Studio/index.js
@@ -0,0 +1,37 @@
+console.log("La suma es " +(2 + 4))
+console.log("La resta es " +(4 - 2))
+console.log("La division es " +(4/2))
+console.log("la multiplicacion es " +(4*2))
+let numero1 = 4
+let numero2 = 2
+let suma = numero1 + numero2
+let resta2 = numero1 - numero2
+console.log("la suma es " +suma)
+alert("la suma es "+ suma)
+console.log("la resta es " +resta2)
+alert("la resta es " +resta2)
+let division = numero1 / numero2
+console.log("la division es " +division)
+alert("la division es " +division)
+let multiplicacion = numero1 * numero2
+console.log("la multiplicacion es " +multiplicacion)
+alert("la multiplicacion es " +multiplicacion)
+let añoactual = 2024
+let añonacimiento = 2010
+let resultado = añoactual - añonacimiento 
+alert("mi edad es " +resultado)
+let añoactual2 = Number(prompt("Ingrese el año actual"))
+let añonacimiento2 = Number(prompt("Ingrese su año de nacimiento"))
+let resultado2 = añoactual2 - añonacimiento2
+alert("Bienvenido")
+alert("Su edad es de " + resultado2)
+let nombrealumno = prompt("Ingrese su nombre")
+alert("Bienvenido " +nombrealumno)
+let apellidoalumno = prompt("Ingrese su apellido")
+alert("Bienvenido " + apellidoalumno + nombrealumno)
+console.log("Bienvenido " + apellidoalumno + nombrealumno)
+let diez= 10
+let tres= 3
+let resultadomultiplicacion = diez * tres
+console.log("Resultado multiplicacion " +  resultadomultiplicacion)
+alert("resultado multiplicacion " + resultadomultiplicacion)
